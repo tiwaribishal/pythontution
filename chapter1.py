a4ad0086f8d1d138ef732afcd9fbc898b429da06
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-12-03 18:00:12 +0530

    Add files via upload

---
+++ chapter1.py
@@ -0,0 +1,29 @@
+#    chapter 1
+''' module , comments & pip  '''
+
+print("hello world")
+
+
+'''                                      practiec question                    '''
+
+
+# wite twinkle twinkle little star poem
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+
+
+#use repl & print table of 5
+'''write in terminal
+  5*1'''
+
+#instal an external module and use it perform different operation
+''' pip instal playsound ( in terminal)
+from playsound import playsound
+playsound ("path ")'''
+
+# write a python program to print the content of a directory using os module search onlinr for the function with does that
+import os
+print(os.listdir())
