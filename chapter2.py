46e75b911cdd80daf090174fb6baab38ab7a402d
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-12-06 22:49:28 +0530

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,101 @@
+   #  chapter 2  
+
+'''        variable and data types                 '''
+
+a = "bishal"
+b=58
+c=76.8
+d=True
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+
+a=45
+b=65
+print("the value of a+b is",a+b)
+print(a-b)
+print(a/b)
+
+
+'''                                        operation                  '''
+'''arithmetical operation'''
+a=67   
+a+=97
+print(a)
+
+'''assignmental operation'''
+a=87        
+a-=8
+print(a)
+
+'''comparision operation'''
+a=(7<=8)
+print(a) 
+
+a=(4!=9)
+print(a)
+
+'''logical operation'''
+bool1=True
+bool2=False
+print((bool1 and bool2))
+'''    types   of    data    types'''
+a= int("878")
+print(type(a))
+
+a=str(787)
+print(type(a))
+
+a=float(66676)
+print(a)
+
+
+
+'''       input           '''
+
+a=input(" enter your name ")
+
+
+
+
+
+'''            practice  question    '''
+
+# add two number 
+a=65
+b=67
+print(a+b)
+
+#find remainder when its divided
+a=7
+b=4
+print(a%b)
+
+
+# use comparision operators to find the a is greater than b or not
+a=(78>=87)
+print(a)
+
+#write a program two  no. enter by user and find average
+a=int(input("enter first no."))
+b=int(input("enter second number "))
+average=a+b/2
+print(average)
+
+
+#write a prograam to find the square  of no
+a=int(input("enter a no. "))
+print(a*a)
+
+
+
+
+
+
+
+
+
